7b80f80c6982ee1f198a1079c8a0df01f5a072bc
Bas Maw <115073747+Bas-Maw-Dev@users.noreply.github.com>
2024-04-23 11:08:57 +0100

    TEST: RED - rows correct for 2 x 2 matrix

---
+++ README.md
@@ -0,0 +1,10 @@
+# Matrix TDD
+
+## Purpose
+To create a matrix that can accept any **n x **n matrix and have a value of the rows and columns.
+
+## Tun tests
+
+```ruby
+$ bin/rspec
+```
---
+++ spec/matrix_spec.rb
@@ -0,0 +1,32 @@
+class Matrix
+  def initialize(string)
+    @string = string
+  end
+  def rows
+    @string
+  end
+
+  def columns
+    @string
+  end
+
+end
+
+describe 'Matrix accepts a (1 x 1) grid' do
+  matrix = Matrix.new('1')
+  it 'returns correct rows' do
+    expect matrix.rows == [['1']]
+  end
+
+  it 'returns correct columns' do
+    expect matrix.columns == [['1']]
+  end
+end
+
+
+describe 'Matrix accepts a (2 x 2) grid' do
+  matrix = Matrix.new('1 2\n3 4')
+  xit 'returns correct rows for a (2 x 2) grid' do
+    expect matrix.rows == [['1', '2'], ['3', '4']]
+  end
+end
