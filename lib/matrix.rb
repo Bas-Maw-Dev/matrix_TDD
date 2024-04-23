fb2e4ab3fc31f5dbc469bb01949df0055965df8b
Bas Maw <115073747+Bas-Maw-Dev@users.noreply.github.com>
2024-04-23 14:41:23 +0100

    TEST: GREEN - returns columns for 2 x 2 matrix

--- lib/matrix.rb
+++ lib/matrix.rb
@@ -13,6 +13,12 @@ class Matrix
   end
 
   def columns
-    [[@string]]
+    rows = self.rows
+    number_of_coloumns = rows[0].length
+    columns = Array.new(number_of_coloumns) { [] }
+    rows.each do |row|
+      row.each_with_index { |element, index| columns[index] << element }
+    end
+    columns
   end
 end
